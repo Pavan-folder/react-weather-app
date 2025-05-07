8cead95e16038703b3cc4c1fcdb858a71a6ec886
Pavan <117636377+Pavan-folder@users.noreply.github.com>
2025-05-07 15:56:41 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "App.jsx";
+import ReactDOM from "react-dom/client"; // React 18+ uses this API
+import App from "App.jsx"; // importing your main App component
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
